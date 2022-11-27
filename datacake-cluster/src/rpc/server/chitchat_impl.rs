62236bd7bb1045d6e89bda2545733f7b3eb34de3
ChillFish8 <hburt2003@gmail.com>
2022-11-27 13:40:57 +0000

    Add chitchat test

--- datacake-cluster/src/rpc/server/chitchat_impl.rs
+++ datacake-cluster/src/rpc/server/chitchat_impl.rs
@@ -31,11 +31,40 @@ impl ChitchatTransport for ChitchatService {
             .map_err(|e| Status::internal(e.to_string()))?;
 
         let mut buffer = msg.message.as_slice();
-        let msg = chitchat::ChitchatMessage::deserialize(&mut buffer)
+        let msg = ChitchatMessage::deserialize(&mut buffer)
             .map_err(|e| Status::internal(e.to_string()))?;
 
         let _ = self.messages.try_send((from, msg));
 
         Ok(Response::new(Empty {}))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::net::{IpAddr, Ipv4Addr};
+    use super::*;
+
+    #[tokio::test]
+    async fn test_chitchat_service() {
+        let (tx, rx) = flume::bounded(10);
+        let service = ChitchatService::new(tx);
+
+        let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::from([127, 0, 0, 1])), 80);
+        let message = ChitchatMessage::BadCluster;
+
+        let msg_req = Request::new(ChitchatRpcMessage {
+            message: message.serialize_to_vec(),
+            source: addr.serialize_to_vec(),
+        });
+
+        service
+            .send_msg(msg_req)
+            .await
+            .expect("Send message");
+
+        let (source, msg) = rx.try_recv().expect("Message should be registered");
+        assert_eq!(source, addr);
+        assert_eq!(msg, message);
     }
 }
