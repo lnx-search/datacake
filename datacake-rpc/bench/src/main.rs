5b3314858bf4c9abecbf2544f19999af8482ef85
Harri <hburt2003@gmail.com>
2022-12-23 23:46:10 +0000

    Add tests and fix bugs

--- datacake-rpc/bench/src/main.rs
+++ datacake-rpc/bench/src/main.rs
@@ -18,105 +18,20 @@ pub mod echo {
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
     let task = tokio::spawn(run_tonic_server());
-    let mut client = EchoClient::connect("http://127.0.0.1:8001").await?;
 
-    let start = Instant::now();
-    //for _ in 0..10_000 {
-        let request = tonic::Request::new(Message {
-            name: "Harrison".into(),
-            age: 19,
-            buffer: vec![0u8; 32 << 20],
-        });
-
-        let response = black_box(client.echo(black_box(request)).await)?;
-        let inner = response.into_inner();
-        assert_eq!(inner.name, "Harrison");
-        assert_eq!(inner.age, 19);
-    //}
-    println!("Tonic took: {:?}, {:?}/req", start.elapsed(), start.elapsed() / 10_000);
-
-    //let client = Endpoint::from_static("http://127.0.0.1:8001").connect().await?;
-    //let start = Instant::now();
-    //let mut tasks = Vec::new();
-    //for _ in 0..100 {
-    //    let mut client = EchoClient::new(client.clone());
-    //    let task = tokio::spawn(async move {
-    //        for _ in 0..10_000 {
-    //            let request = tonic::Request::new(Message {
-    //                name: "Harrison".into(),
-    //                age: 19,
-    //                buffer: vec![0u8; 32 << 20],
-    //            });
-    //
-    //            let response = black_box(client.echo(black_box(request)).await)?;
-    //            let inner = response.into_inner();
-    //            assert_eq!(inner.name, "Harrison");
-    //            assert_eq!(inner.age, 19)
-    //        }
-    //        Ok::<_, anyhow::Error>(())
-    //    });
-    //    tasks.push(task);
-    //}
-    //
-    //for task in tasks {
-    //    task.await.unwrap()?;
-    //}
-    //println!("(100 Concurrency) Tonic took: {:?}, {:?}/req", start.elapsed(), start.elapsed() / (10_000 * 100));
-    task.abort();
-
-    let bind = "0.0.0.0:8000".parse::<SocketAddr>().unwrap();
-    let connect = "127.0.0.1:8000".parse::<SocketAddr>().unwrap();
+    let bind = "0.0.0.0:9000".parse::<SocketAddr>().unwrap();
     let server = datacake_rpc::Server::listen(bind).await.unwrap();
     server.add_service(MyService);
     println!("Listening to address {}!", bind);
 
-    let client = datacake_rpc::Client::connect(connect).await.unwrap();
-    println!("Connected to address {}!", connect);
+    task.await.unwrap()?;
 
-    let mut rpc_client = client.create_rpc_client::<MyService>().await.unwrap();
-    let start = Instant::now();
-    //for _ in 0..10_000 {
-        let request = DatacakeMessage {
-            name: "Harrison".into(),
-            age: 19,
-            buffer: vec![0u8; 32 << 20],
-        };
-        let response = black_box(rpc_client.send(&black_box(request)).await)?;
-        assert_eq!(response.name, "Harrison");
-        assert_eq!(response.age, 19);
-    //}
-    println!("Datacake took: {:?}, {:?}/req", start.elapsed(), start.elapsed() / 10_000);
-
-    //let start = Instant::now();
-    //let mut tasks = Vec::new();
-    //for _ in 0..100 {
-    //    let mut rpc_client = rpc_client.clone_client().await.unwrap();
-    //    let task = tokio::spawn(async move {
-    //        for _ in 0..10_000 {
-    //            let request = DatacakeMessage {
-    //                name: "Harrison".into(),
-    //                age: 19,
-    //                buffer: vec![0u8; 32 << 20],
-    //            };
-    //            let response = black_box(rpc_client.send(&black_box(request)).await)?;
-    //            assert_eq!(response.name, "Harrison");
-    //            assert_eq!(response.age, 19)
-    //        }
-    //        Ok::<_, anyhow::Error>(())
-    //    });
-    //    tasks.push(task);
-    //}
-    //
-    //for task in tasks {
-    //    task.await.unwrap()?;
-    //}
-    //println!("(100 Concurrency) Datacake took: {:?}, {:?}/req", start.elapsed(), start.elapsed() / (10_000 * 100));
     Ok(())
 }
 
 
 async fn run_tonic_server() -> Result<()> {
-    let addr = "127.0.0.1:8001".parse().unwrap();
+    let addr = "0.0.0.0:9001".parse().unwrap();
     let greeter = TonicServer::default();
 
     println!("Tonic server listening on {}", addr);
